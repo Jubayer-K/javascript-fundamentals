71b4445193b485eec8874e5bd3ee930d44e1d1fe
Jubayer-K <jubayerkhan21@gmail.com>
2024-01-28 23:15:58 +0600

    added busFareCalculator solution

--- ternaryNumberSolution.js
+++ ternaryNumberSolution.js
@@ -3,16 +3,15 @@ let num2 = 66;
 
 let result;
 
-if(num1>num2){
-    result = num1*2;
-    console.log(result);
-}else{
-    result = num1 + num2;
-    console.log(result);
+if (num1 > num2) {
+  result = num1 * 2;
+  console.log(result);
+} else {
+  result = num1 + num2;
+  console.log(result);
 }
 
 //using ternary operator
 
-num1>num2 ? result = num1*2 : result = num1 + num2;
-
+num1 > num2 ? (result = num1 * 2) : (result = num1 + num2);
 console.log(result);
