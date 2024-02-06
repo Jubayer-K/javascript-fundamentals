e3701d9b8a895f0db5651ffd1071e5a62887580f
Jubayer-K <jubayerkhan21@gmail.com>
2024-02-07 05:18:37 +0600

    added sum solution

--- js-loop-tasks/break/2 sum upto 100.js
+++ js-loop-tasks/break/2 sum upto 100.js
@@ -1,3 +1,12 @@
 /* 
 Write a while loop that adds numbers starting from 1, but stops (using break) as soon as the sum reaches or exceeds 100
 */
+let num = 0;
+let sum = 1;
+while (true) {
+  sum = sum + num;
+  if (sum >= 100) {
+    break;
+  }
+  num++;
+}
---
+++ js-loop-tasks/break/sum100Solution.js
@@ -0,0 +1,10 @@
+let num = 1;
+let sum = 0;
+while (true) {
+  sum = sum + num;
+  if (sum >= 100) {
+    break;
+  }
+  num++;
+}
+console.log(num);
